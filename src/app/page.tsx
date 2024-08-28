77c2df8e9a36b8d34e40e7b056231db943d2fbf4
Yuto Urashima <yuto.urashima24@gmail.com>
2024-08-28 22:07:07 +0900

    feat: 日付を右上に追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,11 @@
 import PomodoroTimer from '@/components/pomodoroTimer'
 
 export default function Home() {
+  const today = new Date().toDateString()
+
   return (
     <div className="flex flex-col justify-center h-screen bg-gradient-to-br from-blue-500 to-blue-400 text-white">
+      <div className="absolute top-0 right-0 p-6">{today}</div>
       <PomodoroTimer />
     </div>
   )
